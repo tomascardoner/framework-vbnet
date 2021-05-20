ca296d986b8ff4b8ee489e3c49fbb7564d0a2b99
Tomás A. Cardoner <tomas@cardoner.com.ar>
2021-05-19 23:26:20 -0300

    Update Constants.vb

--- Constants.vb
+++ Constants.vb
@@ -17,14 +17,17 @@ Namespace CardonerSistemas
         Friend Const FIELD_VALUE_NOTSPECIFIED_SHORT As Short = 0
         Friend Const FIELD_VALUE_NOTSPECIFIED_INTEGER As Integer = 0
         Friend Const FIELD_VALUE_NOTSPECIFIED_DATE As Date = #12:00:00 AM#
+        Friend Const FIELD_VALUE_NOTSPECIFIED_STRING As String = ""
 
         Friend Const FIELD_VALUE_ALL_BYTE As Byte = Byte.MaxValue
         Friend Const FIELD_VALUE_ALL_SHORT As Short = Short.MaxValue
         Friend Const FIELD_VALUE_ALL_INTEGER As Integer = Integer.MaxValue
+        Friend Const FIELD_VALUE_ALL_STRING As String = "#"
 
         Friend Const FIELD_VALUE_OTHER_BYTE As Byte = Byte.MaxValue - 1
         Friend Const FIELD_VALUE_OTHER_SHORT As Short = Short.MaxValue - 1
         Friend Const FIELD_VALUE_OTHER_INTEGER As Integer = Integer.MaxValue - 1
+        Friend Const FIELD_VALUE_OTHER_STRING As String = "@"
 
         '//////////////////
         '    STRINGS
